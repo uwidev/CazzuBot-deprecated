91a58559412264106dd5e9e5e981ee37b20949b7
usaradark <gamekid263@gmail.com>
2018-08-22 01:38:05 -0700

    Another major overhaul on xml data storage and revamp of how userauth functions. Still need userauth clear.

--- cogs/automations.py
+++ cogs/automations.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as ET
 
 class AutomationsCog():
     def __init__(self, bot):
-        self.bot = bot 
+        self.bot = bot
 
     async def on_raw_reaction_add(self, payload):
         tree = ET.parse('server_data/{}/config.xml'.format(payload.guild_id))
@@ -12,30 +12,34 @@ class AutomationsCog():
         # userauth
         try:
             userauth = tree.find('userauth')
+            xml_message = userauth.find('message')
+            xml_emoji = userauth.find('emoji')
+            xml_role = userauth.find('role')
             guild = self.bot.get_guild(payload.guild_id)
 
-            if payload.message_id == int(userauth.get('MessageID')) and payload.emoji.id == int(userauth.get('Emoji')) and userauth.get('Status') == 'Enabled':
-                #await self.bot.get_channel(payload.channel_id).send('Success!')
-                await guild.get_member(payload.user_id).add_roles(discord.utils.get(guild.roles, id = int(userauth.get('AuthRoleID'))))
+            #await guild.get_channel(payload.channel_id).send('{} {}'.format(payload.emoji.id, xml_emoji.find('id').text))
+            if payload.message_id == int(xml_message.find('id').text) and str(payload.emoji) == xml_emoji.find('id').text:
+                await guild.get_member(payload.user_id).add_roles(discord.utils.get(guild.roles, id = int(xml_role.find('id').text)))
+
         except ValueError:
             print("userauth settings not properly defined")
 
-        # automated user self-roles
-        s_rolelist = tree.find('selfroles')
-        selfroles_msg_id = int(s_rolelist.find('msg_id').get('Value'))
-        selfroles_roles_ch = int(s_rolelist.find('ch_id').get('Value'))
-        guild = self.bot.get_guild(payload.guild_id)
-        
-        if payload.message_id == int(userauth.get('MessageID')) and payload.emoji.id == int(userauth.get('Emoji')) and userauth.get('Status') == 'Enabled':
-            #await self.bot.get_channel(payload.channel_id).send('Success!')
-            await guild.get_member(payload.user_id).add_roles(discord.utils.get(guild.roles, id = int(userauth.get('AuthRoleID'))))
-            
-        if payload.message_id == selfroles_msg_id and payload.channel_id == selfroles_roles_ch:  # and s_rolelist.get('Status') == 'Enabled':
-            for e in s_rolelist.iter():
-                if(payload.emoji.name == e.tag):
-                    await guild.get_member(payload.user_id).add_roles(
-                        discord.utils.get(guild.roles, name=e.get("Role")))
-                    break
+        # # automated user self-roles
+        # s_rolelist = tree.find('selfroles')
+        # selfroles_msg_id = int(s_rolelist.find('msg_id').get('Value'))
+        # selfroles_roles_ch = int(s_rolelist.find('ch_id').get('Value'))
+        # guild = self.bot.get_guild(payload.guild_id)
+        #
+        # if payload.message_id == int(userauth.get('MessageID')) and payload.emoji.id == int(userauth.get('Emoji')) and userauth.get('Status') == 'Enabled':
+        #     #await self.bot.get_channel(payload.channel_id).send('Success!')
+        #     await guild.get_member(payload.user_id).add_roles(discord.utils.get(guild.roles, id = int(userauth.get('AuthRoleID'))))
+        #
+        # if payload.message_id == selfroles_msg_id and payload.channel_id == selfroles_roles_ch:  # and s_rolelist.get('Status') == 'Enabled':
+        #     for e in s_rolelist.iter():
+        #         if(payload.emoji.name == e.tag):
+        #             await guild.get_member(payload.user_id).add_roles(
+        #                 discord.utils.get(guild.roles, name=e.get("Role")))
+        #             break
 
     async def on_raw_reaction_remove(self, payload):
         tree = ET.parse('server_data/{}/config.xml'.format(payload.guild_id))
@@ -51,7 +55,7 @@ class AutomationsCog():
                     await guild.get_member(payload.user_id).remove_roles(
                         discord.utils.get(guild.roles, name=e.get("Role")))
                     break
-    
+
 '''
     async def on_message(self, message):
         await self.bot.process_commands(message)
@@ -59,14 +63,14 @@ class AutomationsCog():
 
         if tree.find('channelvotes').get('Status') == 'Enabled':
             voteon = list((int(element.get('Channel_ID')) for element in tree.iter('voteon')))
-            
+
             if message.channel.id in voteon:
                 print('trueee')
                 await message.add_reaction('\U0001f44d')
                 await message.add_reaction('\U0001f44e')
                 await message.add_reaction('\U0001f914')
 '''
-             
+
 
 def setup(bot):
     bot.add_cog(AutomationsCog(bot))
---
+++ cogs/help.py
@@ -0,0 +1,92 @@
+import xml.etree.ElementTree as ET
+import discord
+from discord.ext import commands
+import emoji
+
+
+def emoji_regional_update():
+    regional_emoji = {
+        u'\U0001F1E6': u':regional_indicator_a:',
+        u'\U0001F1E7': u':regional_indicator_b:',
+        u'\U0001F1E8': u':regional_indicator_c:',
+        u'\U0001F1E9': u':regional_indicator_d:',
+        u'\U0001F1EA': u':regional_indicator_e:',
+        u'\U0001F1EB': u':regional_indicator_f:',
+        u'\U0001F1EC': u':regional_indicator_g:',
+        u'\U0001F1ED': u':regional_indicator_h:',
+        u'\U0001F1EE': u':regional_indicator_i:',
+        u'\U0001F1EF': u':regional_indicator_j:',
+        u'\U0001F1F0': u':regional_indicator_k:',
+        u'\U0001F1F1': u':regional_indicator_l:',
+        u'\U0001F1F2': u':regional_indicator_m:',
+        u'\U0001F1F3': u':regional_indicator_n:',
+        u'\U0001F1F4': u':regional_indicator_p:',
+        u'\U0001F1F5': u':regional_indicator_o:',
+        u'\U0001F1F6': u':regional_indicator_r:',
+        u'\U0001F1F7': u':regional_indicator_s:',
+        u'\U0001F1F8': u':regional_indicator_t:',
+        u'\U0001F1F9': u':regional_indicator_q:',
+        u'\U0001F1FA': u':regional_indicator_u:',
+        u'\U0001F1FB': u':regional_indicator_v:',
+        u'\U0001F1FC': u':regional_indicator_w:',
+        u'\U0001F1FD': u':regional_indicator_x:',
+        u'\U0001F1FE': u':regional_indicator_y:',
+        u'\U0001F1FF': u':regional_indicator_z:'
+        }
+
+    emoji.UNICODE_EMOJI.update(regional_emoji)
+
+
+class AllEmoji(commands.EmojiConverter):
+    async def convert(self, ctx, argument):
+        if await is_custom_emoji(argument):
+            return await super().convert(ctx, argument)
+        return argument
+
+
+async def is_custom_emoji(argument):
+    if argument in emoji.UNICODE_EMOJI or argument in emoji.UNICODE_EMOJI_ALIAS:
+        return False
+    return True
+
+
+class StatusStr(commands.Converter):
+    async def convert(self, ctx, argument):
+        accepted = ['enabled', 'disabled']  # User RE to allow present tense, conver to past
+        if argument.lower() in accepted:
+            return argument.lower()
+        raise commands.BadArgument('Accepted values are "enabled" or "disabled".')
+
+
+class me():
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command()
+    async def test(self, ctx, emoji: AllEmoji):
+        print(str(emoji))
+
+
+    async def userauth_to_str(self, root: ET.Element):
+        '''
+        Given the element of userauth as root, returns indented list
+        '''
+        to_return = ''
+        # to_return.append('status: {}'.format(root.find('status').text))
+        to_return += '    role: {}'.format(root.find('role').find('name').text)
+
+        xml_emoji = root.find('emoji')
+        if bool(xml_emoji.find('custom').text) == 'True':
+            to_return += '\n    emoji: {}'.format(str(self.bot.get_emoji(int(xml_emoji.find('id').text))))
+        else:
+            to_return += '\n    emoji: {}'.format(xml_emoji.find('id').text)
+
+        return to_return
+
+
+
+def setup(bot):
+    bot.add_cog(me(bot))
+
+emoji_regional_update()
